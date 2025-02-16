ccc93309fda5f5b89127e267b57d9dde04e54437
HGHyeon <hangh2999@gmail.com>
2025-02-16 19:00:13 +0900

    임시로 comment 비활성화

--- components/comment.tsx
+++ components/comment.tsx
@@ -3,25 +3,29 @@
 // 2. Update repo, repoId, and categoryId with your own giscus settings
 // 3. Visit https://giscus.app to get your settings
 
-import Giscus from "@giscus/react";
+// import Giscus from "@giscus/react";
+
+// export default function Comment() {
+//   return (
+//     <Giscus
+//       id="comments"
+//       repo="HGHyeon/notion_blog"
+//       repoId="R_kgDON6S-zA"
+//       category="Announcements"
+//       categoryId="DIC_kwDON6S-zM4CnAvk"
+//       mapping="pathname"
+//       strict="0"
+//       reactionsEnabled="1"
+//       emitMetadata="0"
+//       inputPosition="top"
+//       theme="preferred_color_scheme"
+//       lang="ko"
+//       loading="lazy"
+//     />
+//   );
+// }
 
 export default function Comment() {
-  return (
-    <Giscus
-      id="comments"
-      repo="HGHyeon/notion_blog"
-      repoId="R_kgDON6S-zA"
-      category="Announcements"
-      categoryId="DIC_kwDON6S-zM4CnAvk"
-      mapping="pathname"
-      strict="0"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme="preferred_color_scheme"
-      lang="ko"
-      loading="lazy"
-    />
-  );
-}
-
+    return null; // 빈 컴포넌트 반환 (아무것도 렌더링하지 않음)
+  }
+  
